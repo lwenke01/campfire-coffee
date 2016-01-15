7d58699a4a110ae823042b1ffe2ce020abe35f38
lwenke01 <lwenke@gmail.com>
2016-01-15 14:28:49 -0800

    fixed table

--- js/chat.js
+++ js/chat.js
@@ -1,10 +1,9 @@
 "use strict";
 
 var timeOfDay = ['6:00 am','7:00 am','8:00 am','9:00 am','10:00 am','11:00 am','12:00 pm','1:00 pm','2:00 pm','3:00 pm','4:00  pm','5:00 pm','6:00 pm','7:00 pm', '8:00 pm']
-var store = [];
+// var kiosks = [pke, cap, spl, slu, sta, web];
 
-
-function coffeeShop(location, minCust, maxCust, aveCups, aveBeans) {
+function CoffeeShop(location, minCust, maxCust, aveCups, aveBeans) {
   this.location = location;
   this.minCust = minCust;
   this.maxCust = maxCust;
@@ -13,99 +12,134 @@ function coffeeShop(location, minCust, maxCust, aveCups, aveBeans) {
   this.hourlyCust = [];
   this.hourlyCups = [];
   this.hourlyBeans = [];
-  this.dailyCups = 0;
-  this.dailyBeans= 0;
+  // this.dailyCups = 0;
+  // this.dailyBeans= 0;
   // this.allDailyBeans = 0
   this.total = 0;
   // this.newStore();
   // kiosks.push(this);
+  this.renderShopRow();
 
 //get random customer number array
-  this.generateHourlyCust = function() {
+  CoffeeShop.prototype.generateRandCust = function() {
   // this.generateHourlyCust = function() {
     for(var i = 0; i < timeOfDay.length; i++) {
       var randCust = Math.floor((Math.random()) * (this.maxCust - this.minCust) + this.minCust);
       this.hourlyCust.push(randCust);
+
     };
   }
 
   //get hourlyCups array
-  this.generateHourlyCups = function() {
+  CoffeeShop.prototype.generateHourlyCups = function() {
     for(var j = 0; j < timeOfDay.length; j++) {
       var cups = this.hourlyCust[j] * this.aveCups;
       //  console.log(parseFloat(cups.toFixed(1)));
        this.hourlyCups.push(cups);
       //  this.dailyCups.push(aveCupsCust);
     // this.allDailyBeans[i] =+ Number((cups / 20).toFixed(2));
-      this.total += cups / 20;
+      // this.total += cups / 20;
     };
   }
-
-  this.generateHourlyBeans = function() {
+//get beans
+  CoffeeShop.prototype.generateHourlyBeans = function() {
     for(var i = 0; i < timeOfDay.length; i++) {
       var beans = this.hourlyCust[i] * this.aveBeans;
       this.hourlyBeans.push(beans);
-      this.dailyBeans += beans;
+      // this.total += beans;
     };
   }
+  CoffeeShop.prototype.generateTotal = function() {
+   return this.total += (cups /20) + beans;
 
+ }
     // this.render =  function() {
     //   this.generateHourlyCust();
     //   this.generateHourlyCups();
     //   this.generateHourlyBeans();
 
-//form
+// data
 coffeeShop.prototype.renderShopRow = function() {
-  this.generateHourlyBeans();
-  var tableDataEl = document.getElementById('tableData');
-//location
+  this.generateTotal();
+  var tableEl = document.getElementById('tableData');
+
   var trEl = document.createElement('tr');
-  var tdEl = document.createElement('th');
+  var thEl = document.createElement('td');
   tdEl.textContent = this.location;
   trEl.appendChild(tdEl);
+  var tdEl2 = document.createElement('td');
+  tdEl2.textContent = 'Total';
+   trEl.appendChild(tdEl2);
+
+  // var tdEl2 = document.createElement('td');
+  //     tdEl.textContent = this.total;
+  //     trEl.appendChild(tdEl2);
 //hour
   for(var i = 0; i < timeOfDay.length; i++) {
     var tdEl = document.createElement('td');
     tdEl.textContent = this.timeOfDay[i];
     trEl.appendChild(tdEl);
   }
-  var tdElem = document.createElement('td');
-      tdEl.textContent = this.total;
-      trEl.appendChild(tdElem);
-
-      tblEl.appendChild(trEl);
-
-
-      function renderHeaderRow() {
-    }
-      tblEl.appendChild(trEl);
-
-    };
-
-    renderHeaderRow();
-
-
-  }
-    var sectEl = document.getElementById('test');
-
-    var tblEl = document.createElement('table');
-      // var tblEl = document.getElementsByClassName('you');
-    var trEl = document.createElement('tr');
-      // trEl = document.getElementsByClassName('hot');
-    var thEl = document.createElement('th');
-    thEl.textContent = 'Store';
-    trEl.appendChild(thEl);
-
-    for(var i = 0; i < timeOfDay.length; i++) {
-      var tdEl = document.createElement('th');
-      tdEl.textContent = timeOfDay[i];
-      trEl.appendChild(tdEl);
+  tableEl.appendChild(trEl);
 }
-    tdEl = document.createElement('th');
-      // tdEl.className = 'for'
-    tdEl.textContent = 'Total';
+
+// headers
+  function renderHeaderRow() {
+    var tableEl = document.getElementById('tableData');
+    var trEl = document.createElement('tr');
+    var thEl = document.createElement('th');
+    thEl.textContent = 'location';
+    thEL.appendChild(trEl);
+
+    var thEl2 = document.createElement('th');
+    thEl2.textContent = 'total';
+    thEl2.appendChild(trEl);
+
+  for(var i = 0; i < timeOfDay.length; i++) {;
+    var tdEl = document.createElement('td');
+    tdEl.textContent = hours[i];
     trEl.appendChild(tdEl);
-    tblEl.appendChild(trEl);
+  }
+  tableDataEl.appendChild(trEl);
+}
+  renderHeaderRow();
+//   var tdElem = document.createElement('td');
+//       tdEl.textContent = this.total;
+//       trEl.appendChild(tdElem);
+//
+//       tblEl.appendChild(trEl);
+//
+//
+//       function renderHeaderRow() {
+//     }
+//       tblEl.appendChild(trEl);
+//
+//     };
+//
+//     renderHeaderRow();
+//
+//
+//   }
+//     var sectEl = document.getElementById('test');
+//
+//     var tblEl = document.createElement('table');
+//       // var tblEl = document.getElementsByClassName('you');
+//     var trEl = document.createElement('tr');
+//       // trEl = document.getElementsByClassName('hot');
+//     var thEl = document.createElement('th');
+//     thEl.textContent = 'Store';
+//     trEl.appendChild(thEl);
+//
+//     for(var i = 0; i < timeOfDay.length; i++) {
+//       var tdEl = document.createElement('th');
+//       tdEl.textContent = timeOfDay[i];
+//       trEl.appendChild(tdEl);
+// }
+//     tdEl = document.createElement('th');
+//       // tdEl.className = 'for'
+//     tdEl.textContent = 'Total';
+//     trEl.appendChild(tdEl);
+//     tblEl.appendChild(trEl);
 
 
   var pke = new Store('Pike Place Market', 14, 55, 1.2, 3.7);
@@ -115,14 +149,14 @@ coffeeShop.prototype.renderShopRow = function() {
   var sta = new Store('Sea-Tac Airport',68, 124, 1.1, 2.7);
   var web = new Store('Website Sales',3, 6, 0, 6.7);
 
-pke.render();
-cap.render();
-spl.render();
-slu.render();
-sta.render();
-web.render();
-
-  sectEl.appendChild(tblEl);
+// pke.render();
+// cap.render();
+// spl.render();
+// slu.render();
+// sta.render();
+// web.render();
+//
+//   sectEl.appendChild(tblEl);
 
 
 //#form
@@ -157,8 +191,8 @@ web.render();
 
 
 
-  }
-
-  // event listener for 'add store button'
+  // }
+  //
+  // // event listener for 'add store button'
   newStoreForm.addEventListener('comment-button', addNewStores);
 //
